75077b31b36bae723abca6f50fb4843cefd36cf0
Derrick <derrickmugisha169@gmail.com>
2025-10-26 20:00:08 +0200

    studies the java for the thried time

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello World!" + " " + name);
+        // comment test in java
+        System.out.println("Hello World!");
+        System.out.print(3);
+        /*
+        multiline comment
+         */
+
+
+    }
+}
